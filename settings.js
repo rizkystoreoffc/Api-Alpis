8de96219e6d79683dc6b7e109a2706b5e1a12c2a
rizkystoreoffc <160393748+rizkystoreoffc@users.noreply.github.com>
2024-03-16 16:27:12 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,23 +19,23 @@ require('dotenv').config()
 
 //_______________________ ┏  Main Setting  ┓ _______________________\\
 
-creator = '乂𝘼𝙡𝙞𝙥乂' // Nama 
+creator = '乂𝙍𝙞𝙯𝙯𝙈𝙤𝙙𝙯乂' // Nama 
 port = 8080 //port host
 LimitApikey = 200 // Limit Apikey default
 
 //___________ ┏  Database Mongodb Setting  ┓ ___________\\
 
-keymongodb = process.env.mongodb //example "mongodb+srv://user:password@name_database.mongodb.net/?retryWrites=true&w=majority";
+keymongodb = mongodb+srv://syakirah3310:N6Eb3way8a1QSUOM@cluster0.7qtn6uk.mongodb.net/?retryWrites=true&w=majority
 
 //___________ ┏  SMTP Setting  ┓ ___________\\
 
 //note service SMTP Gmail Tidak Boleh Send Kat Temp Email Kalau mahu pakai temp email buat di service lain
 usetempemail = false // kalau true boleh sing up pakai email temp kalau pakai service gmail pakai false je 
 servicesmtp = 'Gmail'//Ada gmail ada sendinblue ikut masing2 limit gmail 500 limit sendinblue 300 free. List service smtp : https://nodemailer.com/smtp/well-known/
-sendemail = process.env.sendemail //Email SMTP
-fromsendemail = process.env.fromsendemail // From Email SMTP *kalau takde custom email kena sama email dengan Email SMTP
-sendpwmail = process.env.sendpwmail // Password Email SMTP
-domain = 'alpis.eu.org'// domain website tanpa http or https
+sendemail = no-reply@rizzpedia.biz.id //Email SMTP
+fromsendemail = no-reply@rizzpedia.biz.id // From Email SMTP *kalau takde custom email kena sama email dengan Email SMTP
+sendpwmail = eRHMrkjX6cTuzgG // Password Email SMTP
+domain = 'rizzpedia.biz.id'// domain website tanpa http or https
 
 //_______________________ ┏  Apikey  ┓ _______________________\\
 
